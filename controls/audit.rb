d37e4ce50e6113b41aa10c1ec75e2eab978af158
juju4 <juju4@users.noreply.github.com>
2022-04-30 15:34:04 +0000

    controls : log_file variable, split by distribution, s/list/entries/

--- controls/audit.rb
+++ controls/audit.rb
@@ -5,6 +5,29 @@
 
 title 'audit section'
 
+if os.darwin?
+  log_dir = '/var/audit'
+  log_dir_mode = '0700'
+elsif os.redhat?
+  log_dir = '/var/log/audit'
+  log_dir_mode = '0700'
+  log_file = '/var/log/audit/audit.log'
+  log_file_mode = '0600'
+  log_group = 'root'
+elsif os.family == 'fedora'
+  log_dir = '/var/log/audit'
+  log_dir_mode = '0700'
+  log_file = '/var/log/audit/audit.log'
+  log_file_mode = '0600'
+  log_group = 'root'
+elsif os.debian?
+  log_dir = '/var/log/audit'
+  log_dir_mode = '0750'
+  log_file = '/var/log/audit/audit.log'
+  log_file_mode = '0640'
+  log_group = 'adm'
+end
+
 control 'audit-1.0' do # A unique ID for this control
   impact 0.7 # The criticality, if this control fails.
   title 'auditd should be present'
@@ -51,7 +74,7 @@ control 'audit-2.0' do
   unless os.darwin?
     describe processes('auditd') do
       its('users') { should eq ['root'] }
-      its('list.length') { should eq 1 }
+      its('entries.length') { should eq 1 }
     end
   end
 end
@@ -61,16 +84,23 @@ control 'audit-3.0' do
   title 'auditd should have log files'
   desc 'Ensure auditd logs file are present'
   only_if { !(virtualization.role == 'guest' && (virtualization.system == 'docker' || virtualization.system == 'lxd')) }
+  describe file(log_dir) do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    its('mode') { should cmp log_dir_mode }
+  end
   if os.darwin?
-    describe file('/var/audit') do
-      it { should be_directory }
-      it { should be_owned_by 'root' }
-      its('mode') { should cmp '0700' }
-    end
     describe file('/var/audit/current') do
       it { should be_symlink }
       it { should be_owned_by 'root' }
       its('mode') { should cmp '0440' }
+    end
+  else
+    describe file(log_file) do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      its('mode') { should cmp log_file_mode }
+      its('group') { should eq log_group }
     end
   end
 end
@@ -80,7 +110,7 @@ control 'audit-4.0' do
   title 'auditd updated log files'
   desc 'Ensure auditd logs file were updated less than 900s in the past'
   only_if { !(virtualization.role == 'guest' && (virtualization.system == 'docker' || virtualization.system == 'lxd')) }
-  describe file('/var/audit/current').mtime.to_i do
+  describe file(log_file).mtime.to_i do
     it { should <= Time.now.to_i }
     it { should >= Time.now.to_i - 900 }
   end
